310f1ab95ed284421618ce623d5d5dea1d92004d
Mathurah Ravigulan <mathurah.ravigulan@gmail.com>
2022-01-11 13:25:11 -0800

    Added base for legacy website

---
+++ legacy-website/components/requestGuestBookSign.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./guestbook.module.css"
+import { Button } from "@mui/material";
+
+
+const RequestGuestBookSign = () => {
+  return (
+    <>
+    <div className={styles.guestbookDescription}>
+    <p>Thank you for visiting my corner on the internet. To leave your digital footprint here, please sign by clicking the button below.  By signing, this document will be part of your endorsement history on the Arweave blockchain. </p>
+    <a href="www.google.com">Learn more here</a>
+    <button className={styles.signButton}>
+      Sign here
+    </button>
+    </div>
+    </>
+  );
+};
+
+export default RequestGuestBookSign;
