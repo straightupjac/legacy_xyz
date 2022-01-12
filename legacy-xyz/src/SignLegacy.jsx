2c6ee4e03c4137d65d782c43a5b22d0d78cf9fbf
straightupjac <jdc.jaclyn@gmail.com>
2022-01-12 15:34:59 -0800

    sign legacy remove web3 provider wrapper

--- legacy-xyz/src/SignLegacy.jsx
+++ legacy-xyz/src/SignLegacy.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 import styles from "./guestbook.module.css"
-import { Web3ReactProvider } from '@web3-react/core';
-import { Web3Provider } from '@ethersproject/providers';
+// import { Web3ReactProvider } from '@web3-react/core';
+// import { Web3Provider } from '@ethersproject/providers';
 import Sign from "./Sign";
 
-function getLibrary(provider) {
-  const library = new Web3Provider(provider);
-  library.pollingInterval = 12000;
-  return library;
-}
+// function getLibrary(provider) {
+//   const library = new Web3Provider(provider);
+//   library.pollingInterval = 12000;
+//   return library;
+// }
 
 const SignLegacy = ({projectId}) => {
   return (
-    <>
-    <Web3ReactProvider getLibrary={getLibrary}>
+    // <Web3ReactProvider getLibrary={getLibrary}>
       <div className={styles.guestbookDescription}>
         <p>Thank you for visiting my corner on the internet. To leave your legacy here, please sign by clicking the button below. By signing, this signature will be part of your legacy on the blockchain. </p>
         <a href="/">Learn more here</a>
         <Sign projectId={projectId} />
       </div>
-    </Web3ReactProvider>
-    </>
+    // </Web3ReactProvider>
   );
 };
 
