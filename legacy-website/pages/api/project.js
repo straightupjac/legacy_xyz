6b9465390c424dcb9525eabcfa3dea477d9ea707
straightupjac <jdc.jaclyn@gmail.com>
2022-01-13 23:58:18 -0800

    add nextjs endpoints

---
+++ legacy-website/pages/api/project.js
@@ -0,0 +1,75 @@
+import Cors from 'cors'
+
+// Initializing the cors middleware
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['POST'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  /** validate req type **/
+  if (req.method !== 'POST') {
+    res.status(400).json({ msg: 'Error' });
+    return;
+  }
+
+  const {
+    projectId,
+    projectName,
+    projectWebsite,
+    projectTwitter,
+    projectTags,
+  } = req.body;
+
+  console.log('adding project', projectId);
+
+  if (projCache.has(projectId)) {
+    const project = projCache.get(project);
+    console.log(`already registered project: ${projectId} called ${project.projectName} with the following params: twitter: ${project.projectTwitter}, website: ${project.projectWebsite}`)
+    res.json(
+      {
+        success: false,
+        msg: `already registered project: ${projectId} called ${project.projectName} with the following params: twitter: ${project.projectTwitter}, website: ${project.projectWebsite}`,
+      })
+    return
+  }
+  else {
+    checkIfProjectRegistered(projectId, projectWebsite, projectTwitter).then((result) => {
+      const { registered, msg, project } = result;
+      if (registered) {
+        projCache.set(projectId, project);
+        res.status(400).json(msg);
+      } else {
+        addProject(projectId, projectName, projectWebsite, projectTwitter, projectTags).then((data) => {
+          console.log(`new project: ${projectId}, ${projectName}, ${projectWebsite}, ${projectTwitter}`)
+          res.json(data);
+        }).catch((err) => {
+          console.log(`err @ /project addProject: ${err}`)
+          res.status(500)
+        })
+      }
+    }).catch((err) => {
+      console.log(`err @ /project checkIfProjectRegistered: ${err}`)
+      res.status(500)
+    })
+  }
+}
+
+export default handler
---
+++ legacy-website/pages/api/verify/[handle].js
@@ -0,0 +1,104 @@
+import Cors from 'cors'
+import Twitter from 'twitter'
+// import Redis from 'ioredis';
+import { checkIfVerifiedAr, storeVerificationAr } from '../helpers/arweave';
+
+// const sigCache = new Redis();
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['POST'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+const TWEET_TEMPLATE = "I am verifying for @legacy_xyz. signature:"
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN,
+})
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  /** validate req type **/
+  if (req.method !== 'POST') {
+    res.status(400).json({ msg: 'Error' });
+    return;
+  }
+
+  const { query: { handle } } = req;
+  const {
+    signature,
+  } = req.body;
+
+  console.log('verifying', handle, signature);
+
+  // if (sigCache.has(handle)) {
+  //   console.log(`already verified user: @${handle}`)
+  //   const txId = sigCache.get(handle)
+  //   res.json({ tx: txId })
+  //   return
+  // }
+
+  client.get('statuses/user_timeline', {
+    screen_name: handle,
+    include_rts: false,
+    count: 5,
+    tweet_mode: 'extended',
+  }, (error, tweets, _) => {
+
+    if (!error) {
+      for (const tweet of tweets) {
+        const parsedSignature = tweet.full_text.slice(TWEET_TEMPLATE.length).split(" ")[0];
+        if (tweet.full_text.startsWith(TWEET_TEMPLATE) && (parsedSignature === signature)) {
+          // check to see if already linked
+
+          checkIfVerifiedAr(handle, signature)
+            .then(result => {
+              if (result) {
+                // already linked
+                console.log(`already verified user: @${handle}`)
+                // sigCache.set(handle, result)
+                res.json({ tx: result })
+              } else {
+                // need to link
+                storeVerificationAr(handle, signature)
+                  .then((tx) => {
+                    console.log(`new verified user: @${handle}, ${signature}`)
+                    // sigCache.set(handle, tx)
+                    res.status(201).json(tx)
+                  })
+                  .catch(e => {
+                    console.log(`err @ /verify/:handle : ${e}`)
+                    res.status(500).send(JSON.stringify(e))
+                  });
+              }
+            });
+          return
+        }
+      }
+      res.status(500).json({ message: 'No matching Tweets found' })
+    } else {
+      console.log('verifying error', error);
+      res.status(500).send({ message: 'Twitter Client Internal Error' })
+    }
+  })
+}
+
+export default handler
