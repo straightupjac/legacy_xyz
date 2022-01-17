401b2b23741f3dc02a744603fc5bdd94d8bf85f3
straightupjac <jdc.jaclyn@gmail.com>
2022-01-18 08:09:11 +0900

    update meta

--- legacy-website/components/Meta.js
+++ legacy-website/components/Meta.js
@@ -23,6 +23,7 @@ const Meta = () => {
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://twitter.com/legacy_xyz" />
       <meta property="twitter:title" content="Leave your web3 legacy. A new era of social proof, and discovery of content." />
+      <meta property="twitter:image" content="/social-preview.png" />
     </Head>
   );
 };
